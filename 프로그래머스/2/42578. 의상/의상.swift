f5ba73ec03c9bcfee8c5405a14e8816fb0eee645
이재영 <ldc1000@naver.com>
2024-11-18 09:35:35 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func solution(_ clothes:[[String]]) -> Int {
+    
+    //옷 종류별 Set 형태로 저장
+    var clothesDict: [String: Set<String>] = [:]
+     
+    for clothe in clothes {
+        let clotheStyle = clothe[0]
+        let clotheType = clothe[1]
+        
+        clothesDict[clotheType, default: []].insert(clotheStyle)
+    }
+    
+    //곱연산을 위해 1로 초기화
+    var countOfStyle = 1
+
+    //옷 종류별 (count + 1) 곱한다 ( +1 : 입지 않은 경우 ) 
+    clothesDict.keys.forEach {
+       countOfStyle = countOfStyle * (clothesDict[$0, default: []].count + 1)
+    }
+    
+    //아무것도 입지 않은 경우 제외 
+    countOfStyle -= 1
+    
+    return countOfStyle
+}
