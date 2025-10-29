7a11d5fc4628cc12873d0fe62fd8949131ef061f
이재영 <ldc1000@naver.com>
2025-10-29 11:05:27 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    var sum = 0
+    var num = n
+    
+    while num != 0 {
+        sum += num % 10 
+        num /= 10
+    }
+    
+    return sum
+}
