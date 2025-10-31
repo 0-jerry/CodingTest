90ab6af3c969a777a1332598a2b90e7b6f4adde2
이재영 <ldc1000@naver.com>
2025-10-31 11:56:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 362.66 ms, Memory: 25.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    let numbers = number.compactMap{ Int(String($0))}
+
+    var sub: [Int] = []
+
+    var k = k
+
+    for (i,e) in numbers.enumerated() {
+        while k>0, sub.isEmpty == false, sub.last! < e {
+            k-=1
+            sub.removeLast()
+        }
+
+        if k == 0 {
+            sub.append(contentsOf: numbers[i...])
+            break
+        } else {
+            sub.append(e)
+        }
+    }
+
+    return sub[..<(sub.count-k)].map {String($0)}.joined()
+}
