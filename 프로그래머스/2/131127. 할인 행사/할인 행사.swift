de31098fd520fe4b24f9fa0b72a437e5fdce9ee6
이재영 <ldc1000@naver.com>
2025-11-05 19:41:15 +0900

    [level 2] Title: 할인 행사, Time: 74.30 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.swift
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -1,24 +1,25 @@
 import Foundation
 
 func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
-    
     var answer = 0
-    var wantDict: [String: Int] = [:]
+    var wantDict = [String: Int]()
     
     for i in 0..<want.count {
         wantDict[want[i]] = number[i]
     }
-        
     
-    for i in 0..<(discount.count - 9) {
-        var discountDict: [String: Int] = [:]
-        for j in 0...9 {
-            discountDict[discount[i+j]] = (discountDict[discount[i+j]] ?? 0) + 1
+    var discountDict = [String: Int]()
+    
+    for i in 0..<discount.count {
+        discountDict[discount[i], default: 0] += 1 
+        if i >= 10 { 
+            if discountDict[discount[i-10]] == 1 { 
+                discountDict[discount[i-10]] = nil 
+            } else {
+            discountDict[discount[i-10], default: 0] -= 1 
+            }
         }
-        
-        if wantDict == discountDict {
-            answer += 1
-        }
+        if wantDict == discountDict { answer += 1 }
     }
     
     return answer
