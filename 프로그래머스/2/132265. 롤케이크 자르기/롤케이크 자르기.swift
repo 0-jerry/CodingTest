724e99a9d5d8ca5c9811589ab526fd5c0a6edd55
이재영 <ldc1000@naver.com>
2025-11-07 12:07:40 +0900

    [level 2] Title: 롤케이크 자르기, Time: 359.39 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -1,22 +1,28 @@
 import Foundation
-/*
-1. 토핑의 수를 Dictionary로 소유하고 갯수를 체크
-2. 갯수가 같을 때부터 +1 카운트. 성립되지 않는 포인트를 발견하면 return
-*/
+
 func solution(_ topping:[Int]) -> Int {
-    var front = [Int: Int]() 
-    var back = [Int: Int]()
-    var answer = 0
-    
-    topping.forEach { back[$0, default: 0] += 1 }
-    
-    for item in topping {
-        front[item, default: 0] += 1
-        back[item] = (back[item]! > 1) ? back[item]! - 1 : nil
-        let isEqual = front.keys.count == back.keys.count
-        if isEqual { answer += 1 }
-        if answer != 0 && !isEqual { break }        
+    var topping1 = Set<Int>()
+    var count1 = [Int](repeating: 0, count: 10001)
+
+    var topping2 = Set<Int>(topping)
+    var count2 = [Int](repeating: 0, count: 10001)
+    for i in 0..<topping.count {
+        count2[topping[i]] += 1
     }
-    
-    return answer
+
+    var ans = 0
+    for i in 0..<topping.count-1 {
+        let num = topping[i]
+        topping1.insert(num)
+        count1[num] += 1
+
+        count2[num] -= 1
+        if count2[num] <= 0 { topping2.remove(num) }
+
+        if topping1.count == topping2.count {
+            ans += 1
+        }
+    }
+
+    return ans
 }
