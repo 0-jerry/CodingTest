1704f7c3c83f157dafe69e78bde781bef7d98a6b
이재영 <ldc1000@naver.com>
2026-01-13 15:20:55 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 523.92 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.swift
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.swift
@@ -2,40 +2,42 @@ import Foundation
 
 func solution(_ diffs:[Int], _ times:[Int], _ limit:Int64) -> Int {
     let puzzleCount = diffs.count
-    var retryTimes = [Int](repeating: 0, count: puzzleCount)
-    var baseTime = times[0]
-    var mostDiff = 0
-    retryTimes[0] = times[0]
+    var retryTimes = [Int64](repeating: 0, count: puzzleCount) 
+    var baseTime: Int64 = Int64(times[0])
+    
+    retryTimes[0] = Int64(times[0])
     
     for i in 1..<puzzleCount {
-        let current = times[i]
-        retryTimes[i] = current + times[i-1]
+        let current = Int64(times[i])
+        retryTimes[i] = current + Int64(times[i-1])
         baseTime += current
-        mostDiff = max(mostDiff, diffs[i])
     }
     
-    let remainTime = Int(limit) - baseTime
+    let remainTime = limit - baseTime
     
-    func calculateTotalTime(_ level: Int) -> Int {
-        var total = 0 
+    func calculateTotalTime(_ level: Int) -> Int64 {
+        var total: Int64 = 0
+        
         for i in 0..<puzzleCount {
-            guard remainTime >= total else { break }
-            
             if diffs[i] > level {
-                total += (diffs[i] - level) * retryTimes[i]
+                total += Int64(diffs[i] - level) * retryTimes[i]
+            }
+            
+            if total > remainTime {
+                return total
             }
         }
         
-        return total 
+        return total
     }
     
     var left = 1
-    var right = mostDiff
+    var right = diffs.max() ?? 1
     var answer = right
     
-    while left <= right { 
-        let mid = (left + right) / 2  
-        let totalTime = calculateTotalTime(mid) 
+    while left <= right {
+        let mid = (left + right) / 2
+        let totalTime = calculateTotalTime(mid)
         
         if totalTime <= remainTime {
             answer = mid
