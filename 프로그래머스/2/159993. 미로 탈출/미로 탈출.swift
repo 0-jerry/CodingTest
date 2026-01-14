98ece6d0e678f5ae1e1a1a294759348ffef94f2e
이재영 <ldc1000@naver.com>
2026-01-14 15:49:36 +0900

    [level 2] Title: 미로 탈출, Time: 14.95 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.swift
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.swift
@@ -1,68 +1,78 @@
 import Foundation
 
 func solution(_ maps: [String]) -> Int {    
+    let width = maps[0].count
+    let height = maps.count
+    let area = width * height 
+    
     struct Position: Hashable {
+        
+        typealias Direction = (x: Int, y: Int)
+        static let directions: [Direction] = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        
         let x: Int
         let y: Int
+        
+        init(_ x: Int, _ y: Int) {
+            self.x = x
+            self.y = y
+        }
+        
+        func move(to direction: Direction) -> Position {
+            Position(x + direction.x, y + direction.y)
+        }
+        
     }
     
     var road = Set<Position>()
-    var specialRoad = [Character: Position]()
+    var specialBoard = [Character: Position]()
     
     for y in 0..<maps.count {
         let line = maps[y]
-        var x = -1 
-        for char in line {
-            x += 1
+        for (x, char) in line.enumerated() {
             guard char != "X" else { continue }
-            let currentPosition = Position(x: x, y: y)
+            let currentPosition = Position(x,y)
             road.insert(currentPosition)
-            if char != "O" {
-                specialRoad[char] = currentPosition
-            }
+            if char != "O" { specialBoard[char] = currentPosition }
         }
     }
     
-    guard let start = specialRoad["S"], 
-          let lever = specialRoad["L"], 
-          let exit = specialRoad["E"] else { return -1 }
+    guard let start = specialBoard["S"], 
+          let lever = specialBoard["L"], 
+          let exit = specialBoard["E"] else { return -1 }
     
     func length(from startPosition: Position, to endPosition: Position) -> Int? {
         var index = 0 
         var queue: [(position: Position, length: Int)] = [(startPosition, 0)]
-        var visited = Set<Position>()
+        var visited = [Bool](repeating: false, count: area)
         
         while index < queue.count {
             let current = queue[index]
             let currentPosition = current.position
             let currentLength = current.length
-            
-            if currentPosition == endPosition { 
-                return currentLength 
-            }
-            
-            let nextPositions = [
-                (0, 1), 
-                (0, -1), 
-                (-1, 0), 
-                (1, 0)
-            ].map { Position(x: currentPosition.x + $0.0, y: currentPosition.y + $0.1) }
-            .filter { road.contains($0) && !visited.contains($0) }
-
             let nextLength = currentLength + 1
+
+            if currentPosition == endPosition { return currentLength }
             
-            nextPositions.forEach { nextPosition in 
+            for direction in Position.directions {
+                let nextPosition = currentPosition.move(to: direction)
+                let flatIndex = nextPosition.x + nextPosition.y * width
+                
+                guard road.contains(nextPosition) else { continue }
+                guard !visited[flatIndex] else { continue }
+                
                 queue.append((nextPosition, nextLength))
-                visited.insert(nextPosition)
+                visited[flatIndex] = true
             }
+            
             index += 1
         }
         
         return nil 
     }
     
-    guard let first = length(from: start, to: lever),
-          let second = length(from: lever, to: exit) else { return -1 }
+    guard let firstRouteLength = length(from: start, to: lever),
+          let secondRouteLength = length(from: lever, to: exit) else { return -1 }
     
-    return first + second
+    return firstRouteLength + secondRouteLength
 }
