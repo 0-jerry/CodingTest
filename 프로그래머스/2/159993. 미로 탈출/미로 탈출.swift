d4adf5077b65a7b9bac9638e5c83e1d8effcd264
이재영 <ldc1000@naver.com>
2026-01-14 14:49:45 +0900

    [level 2] Title: 미로 탈출, Time: 34.18 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.swift
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.swift
@@ -1,23 +1,5 @@
 import Foundation
-/*
-벽, 통로, 문, 레버
-통로를 따라서 레버를 작동시키고, 문으로 탈출
-이동은 1초 
-S -> L
-L -> E
-두 이동에 대해서 최솟값을 연산해 더한다
 
-A. 좌표 간의 이동을 연산
-    1. 종료 플래그
-        목표위치 도착 - 최솟값 저장
-        재방문 - visited 캐싱
-    2. (Position 과 이동시간) enqueue
-    3. dequeue 한 값 의 상하좌우 visited 확인 후 이동시간 + 1 해 enqueue
-        
-B. 최적화
-    1. Set을 통해 필터링 - 길 검증
-
-*/
 func solution(_ maps: [String]) -> Int {    
     struct Position: Hashable {
         let x: Int
