f36093ba0efcda70b461e72df6ec37f3eb5e7376
이재영 <ldc1000@naver.com>
2026-01-17 14:25:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 193.31 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -1,24 +1,52 @@
+import Foundation
 
-func solution(_ number:String, _ k:Int) -> String {
-    let numbers = number.compactMap{ $0.wholeNumberValue }
-
-    var sub: [Int] = []
-
-    var k = k
-
-    for (i,e) in numbers.enumerated() {
-        while k>0, sub.isEmpty == false, sub.last! < e {
-            k-=1
-            sub.removeLast()
+func solution(_ number: String, _ k: Int) -> String {
+    var removedCount = 0 
+    var stack = Stack()
+    
+    func push(_ value: Character) {        
+        while removedCount < k && stack.isPushable(value) {
+            guard stack.pop() != nil else {
+                break
+            }
+            removedCount += 1
         }
-
-        if k == 0 {
-            sub.append(contentsOf: numbers[i...])
-            break
-        } else {
-            sub.append(e)
-        }
+        
+        stack.push(value)
     }
+    
+    for char in number {
+        push(char)
+    }
+    
+    while removedCount < k {
+        let _ = stack.pop()
+        removedCount += 1
+    }
+    
+    return stack.joined()
+}
 
-    return sub[..<(sub.count-k)].map {String($0)}.joined()
+struct Stack {
+    
+    private var values: [Character] = []
+    
+    func joined() -> String {
+        String(values)
+    }
+    
+    func isPushable(_ value: Character) -> Bool {
+        guard let last = values.last else { return true }
+        return last < value
+    }
+    
+    mutating func push(_ value: Character) {
+        values.append(value)
+    }
+    
+    mutating func pop() -> Character? {
+        guard !values.isEmpty else { return nil }
+        return values.removeLast()
+    }
+    
 }
