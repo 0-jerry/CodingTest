4c16038116cfb57b0873f459b5a557822b2661cc
이재영 <ldc1000@naver.com>
2026-02-06 18:20:39 +0900

    [level 4] Title: 지형 이동, Time: 1469.88 ms, Memory: 22.9 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.swift
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.swift
@@ -1,88 +1,141 @@
 import Foundation
 
-func solution(_ land: [[Int]], _ height: Int) -> Int {
-    let length = land.count
+struct Heap<T> {
+  var nodes: [T] = []
+  let comparer: (T,T) -> Bool
 
-    struct Route {
-        let points: (Int, Int)
-        let cost: Int
+  var isEmpty: Bool {
+      return nodes.isEmpty
+  }
 
-        init(points: (Int, Int), cost: Int) {
-            self.points = (min(points.0, points.1), max(points.0, points.1))
-            self.cost = cost
-        }
-    }
-    
-    let routes: [Route] = { 
-        var routes = [Route]() 
-        var isVisited: [[Int]] = [[Int]](repeating: [], count: length * length)
-        let direction = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+  init(comparer: @escaping (T,T) -> Bool) {
+      self.comparer = comparer
+  }
 
-        func flatIndex(_ x: Int, _ y: Int) -> Int? {
-            guard x >= 0 && x < length && y >= 0 && y < length else {
-                return nil 
-            }
-            return x + y * length
-        }
-        
-        func calCost(_ lhs: (Int, Int), _ rhs: (Int, Int)) -> Int {
-            let sub = abs(land[lhs.1][lhs.0] - land[rhs.1][rhs.0])
-            return sub > height ? sub : 0
-        }
-        
-        func calRoutes(_ x: Int , _ y: Int) -> [Route] {
-            guard let index = flatIndex(x, y) else { return [] }
-            return direction
-            .compactMap { 
-                dirX, dirY in 
-                let newX = x + dirX
-                let newY = y + dirY
-                guard let surround = flatIndex(newX, newY), 
-                      !isVisited[surround].contains(index) else { 
-                    return nil 
-                }
-                let cost = calCost((x, y), (newX, newY))
-                isVisited[index].append(surround)
-                return Route(points: (index, surround), cost: cost)
-            }
-        }
-        
-        for y in 0..<length {
-            for x in 0..<length {
-                calRoutes(x, y).forEach {
-                    routes.append($0)
-                }
-            }
-        }
-        
-        return routes.sorted { $0.cost < $1.cost }
-    }()
-    
-    var parents: [Int] = Array(0...length*length-1)
-    var totalCost = 0
-    
-    func find(_ index: Int) -> Int {
-        if parents[index] == index {
-            return index
-        } else {
-            return find(parents[index])
-        }
+  func peek() -> T? {
+      return nodes.first
+  }
+
+  mutating func insert(_ element: T) {
+      var index = nodes.count
+
+      nodes.append(element)
+
+      while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) {
+          nodes.swapAt(index, (index-1)/2)
+          index = (index-1)/2
+      }
+  }
+
+  mutating func delete() -> T? {
+      guard !nodes.isEmpty else {
+          return nil
+      }
+
+      if nodes.count == 1 {
+          return nodes.removeFirst()
+      }
+
+      let result = nodes.first
+      nodes.swapAt(0, nodes.count-1)
+      nodes.popLast()
+
+      var index = 0
+
+      while index < nodes.count {
+          let left = index * 2 + 1
+          let right = left + 1
+
+          if right < nodes.count {
+              if comparer(nodes[left], nodes[right]),
+                  !comparer(nodes[right], nodes[index]) {
+                  nodes.swapAt(right, index)
+                  index = right
+              } else if !comparer(nodes[left], nodes[index]){
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else if left < nodes.count {
+              if !comparer(nodes[left], nodes[index]) {
+                  nodes.swapAt(left, index)
+                  index = left
+              } else {
+                  break
+              }
+          } else {
+              break
+          }
+      }
+
+      return result
+  }
+}
+
+struct Point: Hashable {
+    let x: Int
+    let y: Int
+}
+
+struct Edge: Hashable, Comparable {
+    let begin: Point
+    let end: Point
+    let cost: Int
+
+    static func < (lhs: Edge, rhs: Edge) -> Bool {
+        return lhs.cost < rhs.cost
     }
-    
-    func combine(_ route: Route) {
-        let lhs = find(route.points.0)
-        let rhs = find(route.points.1)
-        if lhs == rhs {
-            return 
-        } else {
-            parents[rhs] = lhs
-            parents[route.points.0] = lhs
-            parents[route.points.1] = lhs
-            totalCost += route.cost
+}
+
+func solution(_ land:[[Int]], _ height:Int) -> Int {
+    var result = 0
+
+    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: land[0].count), count: land.count)
+
+    var pq: Heap<Edge> = Heap(comparer: >)
+
+    visited[0][0] = true
+
+    pq.insert(Edge(begin: Point(x: 0, y: 0), end: Point(x: 0, y: 1), cost: abs(land[0][0] - land[1][0])))
+    pq.insert(Edge(begin: Point(x: 0, y: 0), end: Point(x: 1, y: 0), cost: abs(land[0][0] - land[0][1])))
+
+    let dx: [Int] = [1,0,-1,0]
+    let dy: [Int] = [0,1,0,-1]
+
+    while let nextEdge = pq.delete() {
+        let x = nextEdge.end.x
+        let y = nextEdge.end.y
+
+        if visited[y][x] {
+            continue
         }
+
+        visited[y][x] = true
+
+        if nextEdge.cost > height {
+            result += nextEdge.cost
+        }
+
+        for i in 0..<4 {
+            let nx = x + dx[i]
+            let ny = y + dy[i]
+
+            if nx<0||nx>=visited[0].count||ny<0||ny>=visited.count {
+                continue
+            }
+
+            if visited[ny][nx] {
+                continue
+            }
+
+            let edge = Edge(begin: nextEdge.end, end: Point(x: nx, y: ny), cost: abs(land[y][x] - land[ny][nx]))
+            pq.insert(edge)
+
+        }
+
     }
-    
-    routes.forEach { combine($0) }
-    
-    return totalCost
+
+
+    return result
 }
