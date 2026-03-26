d7a3b7342fd9d09e40606905be276bb359988d9b
이재영 <ldc1000@naver.com>
2026-03-26 18:31:02 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 155.08 ms, Memory: 22.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.swift
@@ -0,0 +1,73 @@
+import Foundation
+
+typealias Range = (start: Int, end: Int)
+
+func solution(_ gems: [String]) -> [Int] {
+    
+    let allCases: [String] = {
+        var allCases = Set<String>()
+        gems.forEach { allCases.insert($0) }
+        return Array<String>(allCases)
+    }()
+    let slot = Slot(allCases)
+    var answer: Range =  (1, gems.count)
+    
+    func shortRange(_ lhs: Range, _ rhs: Range) -> Range {
+        (lhs.end - lhs.start) <= (rhs.end - rhs.start) ? lhs : rhs
+    }
+    
+    for element in gems {
+        slot.append(element)
+        
+        guard let newAnswer = slot.tightRange() else { continue }
+        answer = shortRange(answer, newAnswer)
+        
+        if (answer.end - answer.start) == allCases.count - 1 { 
+            break 
+        }
+    }
+    
+    return [answer.start, answer.end]
+}
+
+final class Slot {
+    private let allCases: [String]
+    private var elements: [String] = []
+    private var index = 0
+    private var elementsCount: [String: Int] = [:]
+    private var selectedCount: Int = 0
+    var isCompleted: Bool = false
+    
+    private var isClearable: Bool {
+        (elements.count >= index) && 
+        isCompleted && 
+        (elementsCount[elements[index], default: 0] > 1)
+    }
+    
+    init(_ allCases: [String]) {
+        self.allCases = allCases
+    }
+    
+    func append(_ element: String) {
+        elementsCount[element, default: 0] += 1 
+        
+        if elementsCount[element] == 1 {
+            selectedCount += 1
+            isCompleted = (selectedCount == allCases.count)
+        } 
+        
+        elements.append(element)
+    }
+    
+    func tightRange() -> Range? {
+        guard isCompleted else { return nil }
+        
+        while isClearable {
+            elementsCount[elements[index], default: 0] -= 1 
+            index += 1
+        }
+        
+        return (index + 1, elements.count)
+    }
+    
+}
